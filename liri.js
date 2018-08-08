e9f7a62cbe2d94f563bbc8831de270da45d4ebc4
SchneiderMan <jschneid94@gmail.com>
2018-08-08 11:17:40 -0700

    Added comments and fixed twitter function to stop after 20 tweets.

--- liri.js
+++ liri.js
@@ -1,24 +1,22 @@
+// Require node packages
 require("dotenv").config();
-
 const request = require("request");
 const Spotify = require("node-spotify-api")
 const Twitter = require("twitter")
 const fs = require("fs");
 
-// TODO: Add the code required to import the keys.js file and store it in a variable
+// Import the key.js file and store it in a variable
 var key = require("./key");
 
+// Assign client keys to variables
 var spotify = new Spotify(key.spotify);
 var twitter = new Twitter(key.twitter);
 
+// Variables to grab the command and media title arguments in the terminal line
 var command = process.argv[2];
 var media = process.argv[3];
 
-// TODO: Make it so liri.js can take in one of the following commands:
-    // my-tweets
-    // spotify-this-song
-    // movie-this
-    // do-what-it-says
+// If LIRI will respond if it receives one of the follow commands:
 if (command === "movie-this") {
     movieThis(media);
 } else if (command === "spotify-this-song") {
@@ -27,16 +25,23 @@ if (command === "movie-this") {
     doWhatItSays();
 } else if (command === "my-tweets") {
     myTweets();
+} else {
+    console.log("I'm sorry, I don't know that command.");
 }
 
 // FUNCTIONS
-// 'my-tweets`
+//------------------
+
+// Twitter - Returns the most recent tweets from the client account
 function myTweets() {
-    // This will show your last 20 tweets and when they were created at in your terminal/bash window
     var params = {screen_name: 'jschneid94'};
-    twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+    twitter.get('statuses/user_timeline', params, function(error, tweets) {
         if (!error) {
-            for (var i = 0; i < tweets.length; i++) {
+            for (var i = 0; i < 20; i++) {
+                // If there are less than 20 tweets, stop the for loop
+                if (tweets[i] === undefined) { 
+                    break; 
+                }
                 var post = tweets[i];
                 console.log("\nTweet: " + post.text)
                 console.log("Posted On: " + post.created_at);
@@ -45,21 +50,13 @@ function myTweets() {
         }
     });
 }
-// 'spotify-this-song`
+// Spotify - Returns information about a song 
 function spotifyThisSong(song_name) {
-    // This will show the following information about the song in your terminal/bash window
-        // Artist(s)
-        // The song's name
-        // A preview link of the song from Spotify
-        // The album that the song is from
-
     spotify.search({ type: 'track', query: song_name , limit: 3}, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
         var obj = data.tracks.items[0];
-        
         console.log("\nArtist: " + obj.artists[0].name)
         console.log("Song Title: " + obj.name);
         console.log("Link: " + obj.external_urls.spotify);
@@ -67,15 +64,12 @@ function spotifyThisSong(song_name) {
         console.log("\n-----------------");
     });
 }
-
+// OMDB -  Returns information about a movie
 function movieThis(movie_name) {
-
     request("http://www.omdbapi.com/?t=" + movie_name + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
         if (error) {
             return console.log('Error occurred: ' + error);
         }
-
         if (!error && response.statusCode === 200) {
             console.log("\nTitle: " + JSON.parse(body).Title);
             console.log("Year: " + JSON.parse(body).Year);
@@ -90,19 +84,15 @@ function movieThis(movie_name) {
     });
     
 }
-
-// 'do-what-it-says`
+// Do What It Says - Performs the command written in random.txt
 function doWhatItSays() {
-    // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
             return console.log(error);
         }
-
         var dataArr = data.split(",");
         var command = dataArr[0];
         var request = dataArr[1];
-
         if (command === "movie-this") {
             movieThis(request);
         } else if (command === "spotify-this-song") {
