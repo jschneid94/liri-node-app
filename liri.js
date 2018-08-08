556dbcf5106dd33a4bb0f5d08a65b71064c2a595
SchneiderMan <jschneid94@gmail.com>
2018-08-07 22:46:56 -0700

    Created spotify function.

--- liri.js
+++ liri.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 
-// TODO: Add the code required to import the keys.js file and store it in a variable
+const request = require("request");
+const Spotify = require("node-spotify-api")
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// TODO: Add the code required to import the keys.js file and store it in a variable
+require("./key");
+
+var spotify = new Spotify(key.spotify);
+// var client_Twitter = new Twitter(keys.twitter);
 
 var command = process.argv[2];
+var movie = process.argv[3];
+var song = process.argv[3];
 
 // TODO: Make it so liri.js can take in one of the following commands:
     // my-tweets
@@ -14,8 +20,13 @@ var command = process.argv[2];
     // do-what-it-says
 if (command === "movie-this") {
     console.log(command);
-    movieThis();
+    movieThis(movie);
+} else if (command === "spotify-this-song") {
+    console.log(command);
+    spotifyThisSong(song);
 }
+
+// FUNCTIONS
 // 'my-tweets`
 function myTweets() {
     // This will show your last 20 tweets and when they were created at in your terminal/bash window
@@ -27,9 +38,33 @@ function spotifyThisSong(song_name) {
         // The song's name
         // A preview link of the song from Spotify
         // The album that the song is from
+
+    spotify.search({ type: 'track', query: song_name }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        
+        console.log(data);
+        // Do something with 'data'
+    });
 }
 // 'movie-this`
 function movieThis(movie_name) {
+
+    request("http://www.omdbapi.com/?t=" + movie_name + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+    
     // Title of the movie.
         // Year the movie came out.
         // IMDB Rating of the movie.
