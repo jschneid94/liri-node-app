511fdc57ff6f6d71c028f3722384c6ba9acdc9ed
SchneiderMan <jschneid94@gmail.com>
2018-08-08 11:00:31 -0700

    Added twitter command.

--- liri.js
+++ liri.js
@@ -2,16 +2,17 @@ require("dotenv").config();
 
 const request = require("request");
 const Spotify = require("node-spotify-api")
+const Twitter = require("twitter")
+const fs = require("fs");
 
 // TODO: Add the code required to import the keys.js file and store it in a variable
-key = require("./key");
+var key = require("./key");
 
 var spotify = new Spotify(key.spotify);
-// var client_Twitter = new Twitter(keys.twitter);
+var twitter = new Twitter(key.twitter);
 
 var command = process.argv[2];
-var movie = process.argv[3];
-var song = process.argv[3];
+var media = process.argv[3];
 
 // TODO: Make it so liri.js can take in one of the following commands:
     // my-tweets
@@ -19,15 +20,30 @@ var song = process.argv[3];
     // movie-this
     // do-what-it-says
 if (command === "movie-this") {
-    movieThis(movie);
+    movieThis(media);
 } else if (command === "spotify-this-song") {
-    spotifyThisSong(song);
+    spotifyThisSong(media);
+} else if (command === "do-what-it-says") {
+    doWhatItSays();
+} else if (command === "my-tweets") {
+    myTweets();
 }
 
 // FUNCTIONS
 // 'my-tweets`
 function myTweets() {
     // This will show your last 20 tweets and when they were created at in your terminal/bash window
+    var params = {screen_name: 'jschneid94'};
+    twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var post = tweets[i];
+                console.log("\nTweet: " + post.text)
+                console.log("Posted On: " + post.created_at);
+                console.log("\n-----------------");
+            }
+        }
+    });
 }
 // 'spotify-this-song`
 function spotifyThisSong(song_name) {
@@ -44,10 +60,11 @@ function spotifyThisSong(song_name) {
 
         var obj = data.tracks.items[0];
         
-        console.log("Artist: " + obj.artists[0].name)
+        console.log("\nArtist: " + obj.artists[0].name)
         console.log("Song Title: " + obj.name);
         console.log("Link: " + obj.external_urls.spotify);
         console.log("Album: " + obj.album.name);
+        console.log("\n-----------------");
     });
 }
 
@@ -55,8 +72,12 @@ function movieThis(movie_name) {
 
     request("http://www.omdbapi.com/?t=" + movie_name + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
+
         if (!error && response.statusCode === 200) {
-            console.log("Title: " + JSON.parse(body).Title);
+            console.log("\nTitle: " + JSON.parse(body).Title);
             console.log("Year: " + JSON.parse(body).Year);
             console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
             console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
@@ -64,6 +85,7 @@ function movieThis(movie_name) {
             console.log("Language: " + JSON.parse(body).Language);
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("\n-----------------");
         }
     });
     
@@ -72,5 +94,20 @@ function movieThis(movie_name) {
 // 'do-what-it-says`
 function doWhatItSays() {
     // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+        var command = dataArr[0];
+        var request = dataArr[1];
+
+        if (command === "movie-this") {
+            movieThis(request);
+        } else if (command === "spotify-this-song") {
+            spotifyThisSong(request);
+        }
+    });
 }
 
